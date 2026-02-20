37bb3e91ad2efb99a42ddebd2b1e91fa7ff440e6
thinhnq-ibl <nqt909@gmail.com>
2026-02-20 20:56:49 +0700

    update forty-five

--- metric/forty-five-pop.py
+++ metric/forty-five-pop.py
@@ -17,13 +17,13 @@ observed = []
 simulated = []
 
 gpd_real = gpd.read_file("../calculate_in_cell/in_flow_cells_normalized_real.geojson")
-gpd_simulate = gpd.read_file("../calculate_out_cell/in_flow_cells_normalized.geojson")
+gpd_simulate = gpd.read_file("../calculate_in_by_pop/in_flow_cells_distance.geojson")
 
 for index, row in gpd_simulate.iterrows():
     cell_id = row["cell_id"]
     real = gpd_real[gpd_real["cell_id"] == cell_id]
     if len(real) > 0:
-        simulated.append(row["norm_in_amount"])
+        simulated.append(row["norm_total_in"])
         observed.append(real["norm_total_in"].iloc[0])
 
 # 2. Create the plot
@@ -37,9 +37,9 @@ limit_max = max(max(observed), max(simulated))
 plt.plot([limit_min, limit_max], [limit_min, limit_max], color='red', linestyle='--', label='1:1 Line (Perfect Match)')
 
 # 4. Format the plot
-plt.xlabel('Observed Streamflow 2 ($m^3/s$)')
-plt.ylabel('GEOGloWS Simulated Streamflow 2 ($m^3/s$)')
-plt.title('Validation: Observed vs Simulated (GEOGloWS) 2')
+plt.xlabel('Observed Streamflow by pop ($m^3/s$)')
+plt.ylabel('GEOGloWS Simulated Streamflow by pop ($m^3/s$)')
+plt.title('Validation: Observed vs Simulated by pop')
 plt.legend()
 plt.grid(True, linestyle=':', alpha=0.7)
 
